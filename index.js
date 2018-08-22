240e9764dc3ffcfd0a3abfc9e2150bd69b1b9ec8
Annie Eby <eby.annie@gmail.com>
2018-08-22 02:08:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ function destructivelyPrependKitten(name) {
 function destructivelyRemoveLastKitten() {
   return kittens.pop()
 }
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+}
