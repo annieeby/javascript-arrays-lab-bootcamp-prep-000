919f4cd838e7252ed64628652c3976de8afb359e
Annie Eby <eby.annie@gmail.com>
2018-08-22 02:04:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your functions and code here
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(name) {
+function destructivelyAppendKitten(array, name) {
   return array.push(name)
 }
